c902e7fb2c82e8b7f6e9f839e7e4f4e08c445217
Moideen Nazaif VM <nazaifmoid@gmail.com>
2019-05-10 10:51:06 +0530

    Added meme object

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
     @IBOutlet weak var imagePickerView: UIImageView!
     @IBOutlet weak var topTextField: UITextField!
     @IBOutlet weak var bottomTextField: UITextField!
+    @IBOutlet weak var shareButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +27,9 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
         cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
         topTextField.defaultTextAttributes = memeTextAttributedString
         bottomTextField.defaultTextAttributes = memeTextAttributedString
+        if imagePickerView.image == nil {
+            shareButton.isEnabled = false
+        }
         
         topTextField.textAlignment = NSTextAlignment.center
         bottomTextField.textAlignment = .center
@@ -103,6 +107,47 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
     func unsubscribeToKeyboardNotification() {
         NotificationCenter.default.removeObserver(self, name:UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    struct Meme {
+        let topText: String
+        let bottomText: String
+        let originalImage: UIImage
+        let memedImage: UIImage
+    }
+    
+    func generateMemedImage() -> UIImage {
+        
+        self.tabBarController?.tabBar.isHidden = true
+        self.navigationController?.navigationBar.isHidden = true
+        
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
+        let memedImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        self.tabBarController?.tabBar.isHidden = false
+        self.navigationController?.navigationBar.isHidden = false
+        
+        return memedImage
+    }
+    
+    func save() {
+        _ = Meme(
+            topText:self.topTextField.text!,
+            bottomText: self.bottomTextField.text!,
+            originalImage: self.imagePickerView.image!,
+            memedImage: generateMemedImage())
+    }
+    
+    @IBAction func Share(_ sender: Any) {
+        let sharedImage = generateMemedImage()
+        let activityController = UIActivityViewController(activityItems:    [sharedImage], applicationActivities: nil)
+        self.present(activityController, animated: true, completion: nil)
+        
+        activityController.completionWithItemsHandler = { (activity, success, items, error) in
+        self.save()
+        }
     }
 }
 
